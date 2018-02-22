019dfdce59e81993093b917c49f6f25c8b1cb3cc
west-pool <35891486+west-pool@users.noreply.github.com>
2018-02-22 11:51:04 +0000

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -156,7 +156,7 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 48081;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 48082;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x31 ,0x31, 0x31, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Bender's nightmare
   std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121019d1dd14d380eefab21428e4db392ee0fa02eccb43334f04c83d280ea0733bca5";
   uint32_t const GENESIS_NONCE = 11010;
@@ -170,7 +170,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 48084;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 48085;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0x31 ,0x32, 0x32, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807221019d1dd14d380eefab21428e4db392ee0fa02eccb43334f04c83d280ea0733bca6";
     uint32_t const GENESIS_NONCE = 11011;
